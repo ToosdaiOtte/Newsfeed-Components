c1f63c1603f784cc0e49ec4a1d31883aaea45281
Toosdai Otte <Toosdai52Otte@gmail.com>
2019-06-11 21:36:44 -0500

    removed gsap; was breaking webpage

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -6,6 +6,9 @@ const toggleMenu = () => {
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
+// // Menu left slideIn Animation using GSAP *not done*
+// TweenLite.from(menu, 2, {left: 0, ease: Power0.easeNone, y: -500 });
+// TweenLite.to(menu, 2, {left: '110px', ease: Power0.easeNone, y: -500});
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
@@ -14,5 +17,3 @@ menuButton.addEventListener('click', () => {
 })
 
 
-// Menu left slideIn Animation using GSAP *not done*
-TweenLite.to(menu, 2, { ease: Power0.easeNone, y: -500 });
