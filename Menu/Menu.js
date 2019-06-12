ce1b37b099e8be4e18bb72b24e0688e48c4475f9
Toosdai Otte <Toosdai52Otte@gmail.com>
2019-06-11 21:29:47 -0500

    finished MVP, working on stretch

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,18 @@
 
 const toggleMenu = () => {
+  menu.classList.toggle('menu--open');
   // Toggle the "menu--open" class on your menu refence. 
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', () => {
+  toggleMenu();
+})
+
+
+// Menu left slideIn Animation using GSAP *not done*
+TweenLite.to(menu, 2, { ease: Power0.easeNone, y: -500 });
